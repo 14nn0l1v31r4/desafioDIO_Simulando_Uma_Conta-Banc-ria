c88f2a57dca96210f1cb6f6822f74043efc9ef91
Iann Molendolff <iannmolendolff@gmail.com>
2024-07-28 02:01:51 -0300

    projeto banco finalizado

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tc = new Scanner(System.in);
+        double saldo = 25;
+        double valor_solicitado = 18;
+
+        System.out.println("Entre com o valor q deseja retirar:");
+        valor_solicitado = tc.nextDouble();
+
+        if(saldo > valor_solicitado){
+            saldo -= valor_solicitado;
+        }
+        else{
+            System.out.println("Saldo insuficiente: ");
+            System.out.println(saldo);
+        }
+
     }
 }
